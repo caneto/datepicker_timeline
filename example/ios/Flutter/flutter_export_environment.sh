32a78affc6d7ae31025200799e09085cf51d74aa
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-06-09 17:46:29 -0300

    Versão do Dart 3.0.3 / Flutter 3.10.3

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\User\fvm\versions\stable"
+export "FLUTTER_ROOT=e:\fvm\3.10.4"
 export "FLUTTER_APPLICATION_PATH=E:\GitHub\datepicker_timeline\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NUMBER=3"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
